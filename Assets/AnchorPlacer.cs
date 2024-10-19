556a6b2d33ea3d7eb9aeefd539289f6450e4e344
Abegail-Magaling <abegail_magaling@dlsu.edu.ph>
2024-10-19 18:19:35 +0800

    finished

--- Assets/AnchorPlacer.cs
+++ Assets/AnchorPlacer.cs
@@ -1,156 +1,131 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
-using TMPro;
 
 public class AnchorPlacer : MonoBehaviour
 {
-    ARAnchorManager anchorManager;
-    ARPointCloudManager pointCloudManager;
-    bool isPointCloudActive = false;
+    private ARAnchorManager arAnchorManager;
 
-    [SerializeField] private GameObject cubePrefab;
-    [SerializeField] private GameObject spherePrefab;
-    [SerializeField] private GameObject capsulePrefab;
-    [SerializeField] private float forwardOffset;
+    [SerializeField] private GameObject[] objectPrefabs;
+    [SerializeField] private float placementDistance = 2f;
+    [SerializeField] private Slider distanceControlSlider;
+    [SerializeField] private GameObject pointCloudDisplay;
+    [SerializeField] private Toggle pointCloudVisibilityToggle;
+    [SerializeField] private Button removeAllAnchorsButton;
+    [SerializeField] private Button[] selectPrefabButtons;
+    [SerializeField] private GameObject panel;
+    [SerializeField] private TMP_Text slidertext;
 
-    [SerializeField] private Slider slider;
-    [SerializeField] private GameObject optionsPanel;
-    [SerializeField] private TextMeshProUGUI sliderValueText;
-    [SerializeField] private Toggle pointCloudToggle;
+    private List<GameObject> activeAnchors = new List<GameObject>();
+    private GameObject currentPrefab;
 
-    [SerializeField] private Button cubeButton;
-    [SerializeField] private Button sphereButton;
-    [SerializeField] private Button capsuleButton;
-
-    private float spawnOffSet;
-
-    private int activePrefab;
-
-    void Start()
+    // Start is called before the first frame update
+    void Awake()
     {
-        anchorManager = GetComponent<ARAnchorManager>();
-        pointCloudManager = FindAnyObjectByType<ARPointCloudManager>();
-        pointCloudToggle.onValueChanged.AddListener(OnTogglePointCloud);
+        arAnchorManager = GetComponent<ARAnchorManager>();
+        currentPrefab = objectPrefabs[0];
 
-        activePrefab = 3;
+        SetupUIListeners();
+    }
+
+    private void SetupUIListeners()
+    {
+        pointCloudVisibilityToggle.onValueChanged.AddListener(TogglePointCloudDisplay);
+        distanceControlSlider.onValueChanged.AddListener(UpdatePlacementDistance);
+
+        for (int buttonIndex = 0; buttonIndex < selectPrefabButtons.Length; buttonIndex++)
+        {
+            int index = buttonIndex;
+            selectPrefabButtons[buttonIndex].onClick.AddListener(() => SelectPrefabByIndex(index));
+        }
+
+        removeAllAnchorsButton.onClick.AddListener(ClearAllAnchors);
     }
 
     void Update()
     {
-        spawnOffSet = slider.value + forwardOffset;
-        sliderValueText.text = slider.value.ToString();
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        slidertext.text = distanceControlSlider.value.ToString("F1");
+
+        if (Input.touchCount == 0) return;
+
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
         {
-            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            HandleTouchPlacement(touch.position);
+        }
+    }
+
+    private void HandleTouchPlacement(Vector2 screenPosition)
+    {
+        Ray rayFromCamera = Camera.main.ScreenPointToRay(screenPosition);
+        if (Physics.Raycast(rayFromCamera, out RaycastHit hitInfo))
+        {
+            // If an anchored object is hit, remove it.
+            if (hitInfo.collider.CompareTag("AnchoredObject"))
             {
+                RemoveAnchor(hitInfo.collider.gameObject);
                 return;
             }
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                ARAnchor anchor = hit.collider.GetComponentInParent<ARAnchor>();
-
-                if (anchor != null)
-                {
-                    Destroy(anchor.gameObject);
-                    return;
-                }
-            }
-
-            Vector3 spawnPos = Camera.main.transform.position + Camera.main.transform.forward * spawnOffSet;
-            if (activePrefab != 3)
-            {
-                AnchorObject(spawnPos);
-            }
         }
+
+        Vector3 placementPosition = rayFromCamera.GetPoint(placementDistance);
+        CreateAnchorAtPosition(placementPosition);
     }
 
-    private bool IsTouchOverUI(Touch touch)
+    private void CreateAnchorAtPosition(Vector3 position)
     {
-        PointerEventData eventData = new PointerEventData(EventSystem.current);
-        eventData.position = new Vector2(touch.position.x, touch.position.y);
+        GameObject anchorGameObject = new GameObject("AnchorObject");
+        anchorGameObject.transform.position = position;
+        ARAnchor anchor = anchorGameObject.AddComponent<ARAnchor>();
 
-        List<RaycastResult> results = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(eventData, results);
+        GameObject instantiatedObject = Instantiate(currentPrefab, anchor.transform);
+        instantiatedObject.transform.localPosition = Vector3.zero;
+        instantiatedObject.tag = "AnchoredObject";
 
-        return results.Count > 0;
+        activeAnchors.Add(anchorGameObject);
     }
 
-
-    public void AnchorObject(Vector3 worldPos)
+    private void RemoveAnchor(GameObject anchorObject)
     {
-        GameObject newAnchor = new GameObject("NewAnchor");
-        newAnchor.transform.parent = null;
-        newAnchor.transform.position = worldPos;
-        newAnchor.AddComponent<ARAnchor>();
+        activeAnchors.Remove(anchorObject);
+        Destroy(anchorObject);
+    }
 
-        if (activePrefab == 0)
+    private void ClearAllAnchors()
+    {
+        foreach (GameObject anchor in activeAnchors)
         {
-            GameObject obj = Instantiate(cubePrefab, newAnchor.transform);
-            obj.transform.localPosition = Vector3.zero;
-            activePrefab = 3;
+            Destroy(anchor);
         }
-        else if (activePrefab == 1)
+        activeAnchors.Clear();
+    }
+
+    private void UpdatePlacementDistance(float newDistance)
+    {
+        placementDistance = newDistance;
+    }
+
+    private void TogglePointCloudDisplay(bool showPointCloud)
+    {
+        pointCloudDisplay.SetActive(showPointCloud);
+    }
+
+    private void SelectPrefabByIndex(int prefabIndex)
+    {
+        currentPrefab = objectPrefabs[prefabIndex];
+
+        for (int i = 0; i < selectPrefabButtons.Length; i++)
         {
-            GameObject obj = Instantiate(spherePrefab, newAnchor.transform);
-            obj.transform.localPosition = Vector3.zero;
-            activePrefab = 3;
-        }
-        else if (activePrefab == 2)
-        {
-            GameObject obj = Instantiate(capsulePrefab, newAnchor.transform);
-            obj.transform.localPosition = Vector3.zero;
-            activePrefab = 3;
-        }
-
-    }
-
-    public void OnTogglePointCloud(bool pointActive)
-    {
-        pointCloudManager.SetTrackablesActive(pointActive);
-    }
-
-    public void OnMazdaButtonPressed()
-    {
-        activePrefab = 0;
-    }
-
-    public void OnCubeButtonPressed()
-    {
-        activePrefab = 1;
-    }
-
-    public void OnPortalButtonPressed()
-    {
-        activePrefab = 2;
-    }
-
-    public void OnDestroyButtonPressed()
-    {
-        foreach (ARAnchor anchor in anchorManager.trackables)
-        {
-            Destroy(anchor.gameObject);
+            selectPrefabButtons[i].interactable = i != prefabIndex;
         }
     }
 
-    public void OnOptionsButtonPressed()
+    public void activateSettings(GameObject obj)
     {
-        if (optionsPanel.activeInHierarchy == false)
-        {eqeqeqeqeqeqeqeqeqeq
-            optionsPanel.SetActive(true);
-        }
-        else
-        {
-            optionsPanel.SetActive(false);
-        }
+        obj.SetActive(!obj.activeSelf);
     }
-
 }
